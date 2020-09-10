92fc730a392c56b5306871ea08e99bc615de8bc3
Ross Greene <ross.greene@softwire.com>
2020-09-10 10:01:06 +0100

    Book info can be edited

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,4 +24,22 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :author, :isbn)
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update_attributes(book_param)
+      redirect_to action: 'index' #, id: @book
+    else
+      render action: 'edit'
+    end
+  end
+
+  def book_param
+    params.require(:book).permit(:title, :price, :subject_id, :description)
+  end
 end
